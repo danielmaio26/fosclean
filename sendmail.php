49e6d229b699f14a90aa7314cc6b5edd1530a934
Daniel <danielmaio62@gmail.com>
2022-05-03 00:14:13 -0300

    Acomodo texto de los comentarios

--- sendmail.php
+++ sendmail.php
@@ -10,8 +10,6 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-
-
 //Load Composer's autoloader
 require 'vendor/autoload.php';
 
@@ -21,20 +19,20 @@ $mail = new PHPMailer(true);
 try {
     //Server settings
     $mail->SMTPDebug = 0;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.zoho.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'info@fosclean.com';                     //SMTP username
-    $mail->Password   = 'Fos13915563';                               //SMTP password
-    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
-    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->isSMTP();                           //Send using SMTP
+    $mail->Host       = 'smtp.zoho.com';       //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                  //Enable SMTP authentication
+    $mail->Username   = 'info@fosclean.com';   //SMTP username
+    $mail->Password   = 'Fos13915563';         //SMTP password
+    $mail->SMTPSecure = 'tls';                 //Enable implicit TLS encryption
+    $mail->Port       = 587;                   //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
     $mail->setFrom('info@fosclean.com', $nombre);
     $mail->addAddress('info@fosclean.com', $nombre);     //Add a recipient
 
     //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->isHTML(true);                                 //Set email format to HTML
     $mail->Subject = 'Mensaje desde formulario de contacto';
     $mail->Body    = $body;
 
