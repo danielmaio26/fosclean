d49b41c22ffa063bc6da0ccad1bd866c0043f7c7
danielmaio26 <danielmaio@yahoo.com>
2022-06-20 12:27:10 -0300

    modifico datos del recaptcha2

--- sendmail.php
+++ sendmail.php
@@ -1,6 +1,4 @@
 <?php
-
-
 $nombre = $_POST["name"];
 $email = $_POST["email"];
 $mensaje = $_POST["message"];
@@ -19,33 +17,6 @@ require 'vendor/autoload.php';
 $mail = new PHPMailer(true);
 
 try {
-
-	define('CLAVE', '6LfZP6wfAAAAAAotDhCCmHFHd91tkiNrKfVg5En-');
-	
-	$token = $_POST['token'];
-	$action = $_POST['action'];
-	
-	$cu = curl_init();
-	curl_setopt($cu, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
-	curl_setopt($cu, CURLOPT_POST, 1);
-	curl_setopt($cu, CURLOPT_POSTFIELDS, http_build_query(array('secret' => CLAVE, 'response' => $token)));
-	curl_setopt($cu, CURLOPT_RETURNTRANSFER, true);
-	$response = curl_exec($cu);
-	curl_close($cu);
-	
-	$datos = json_decode($response, true);
-	
-	print_r($datos);
-	
-	if($datos['success'] == 1 && $datos['score'] >= 0.5){
-		if($datos['action'] == 'validarUsuario'){
-			
-		}
-		
-		} else {
-		echo "ERES UN ROBOT";
-	}
-
     //Server settings
     $mail->SMTPDebug = 0;                      //Enable verbose debug output
     $mail->isSMTP();                           //Send using SMTP
