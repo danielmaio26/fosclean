ad65494298131cc8e11a930321e93c317dfdd60a
danielmaio26 <danielmaio@yahoo.com>
2022-06-26 19:23:21 -0300

    arreglo problema con recaptcha

--- sendmailV2-prueba.php
+++ sendmailV2-prueba.php
@@ -1,30 +1,30 @@
 <?php
 
-define('CLAVE', '6LfZP6wfAAAAAAotDhCCmHFHd91tkiNrKfVg5En-');
+//define('CLAVE', '6LfZP6wfAAAAAAotDhCCmHFHd91tkiNrKfVg5En-');
 	
-$token = $_POST['token'];
-$action = $_POST['action'];
+//$token = $_POST['token'];
+//$action = $_POST['action'];
 
-$cu = curl_init();
-curl_setopt($cu, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
-curl_setopt($cu, CURLOPT_POST, 1);
-curl_setopt($cu, CURLOPT_POSTFIELDS, http_build_query(array('secret' => CLAVE, 'response' => $token)));
-curl_setopt($cu, CURLOPT_RETURNTRANSFER, true);
-$response = curl_exec($cu);
-curl_close($cu);
+//$cu = curl_init();
+//curl_setopt($cu, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
+//curl_setopt($cu, CURLOPT_POST, 1);
+//curl_setopt($cu, CURLOPT_POSTFIELDS, http_build_query(array('secret' => CLAVE, 'response' => $token)));
+//curl_setopt($cu, CURLOPT_RETURNTRANSFER, true);
+//$response = curl_exec($cu);
+//curl_close($cu);
 
-$datos = json_decode($response, true);
+//$datos = json_decode($response, true);
 
-print_r($datos);
+//print_r($datos);
 
-if($datos['success'] == 1 && $datos['score'] >= 0.5){
-    if($datos['action'] == 'validarUsuario'){
+//if($datos['success'] == 1 && $datos['score'] >= 0.5){
+//    if($datos['action'] == 'validarUsuario'){
         
-    }
+//    }
     
-    } else {
-    echo "ERES UN ROBOT";
-}
+//    } else {
+//    echo "ERES UN ROBOT";
+//}
 
 $nombre = $_POST["name"];
 $email = $_POST["email"];
@@ -40,6 +40,22 @@ use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader
 require 'vendor/autoload.php';
+
+$ip = $_SERVER["REMOTE_ADDR"];
+$captcha = $_POST['g-recaptcha-response'];
+$secretKey = '6LegTIggAAAAAHVbxNpJBYY_m20o5Xgr-oIFSfkh';
+
+$errors = array();
+
+$response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$secretKey}&response={$captcha}&remoteip={$ip}");
+
+$atributos = json_decode($response, TRUE);
+
+$errors = array();
+
+if (!$atributos['success']) {
+    $errors[] = 'Verifica el captcha';
+}
 
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
