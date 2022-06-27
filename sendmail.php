ff1f679514ae0484af1906d911f8454ee175bbae
danielmaio26 <danielmaio@yahoo.com>
2022-06-27 13:28:55 -0300

    corrijo resultadosvalidacion recaptcha

--- sendmail.php
+++ sendmail.php
@@ -1,31 +1,5 @@
 <?php
-
-define('CLAVE', '6LfZP6wfAAAAAAotDhCCmHFHd91tkiNrKfVg5En-');
-	
-$token = $_POST['token'];
-$action = $_POST['action'];
-
-$cu = curl_init();
-curl_setopt($cu, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
-curl_setopt($cu, CURLOPT_POST, 1);
-curl_setopt($cu, CURLOPT_POSTFIELDS, http_build_query(array('secret' => CLAVE, 'response' => $token)));
-curl_setopt($cu, CURLOPT_RETURNTRANSFER, true);
-$response = curl_exec($cu);
-curl_close($cu);
-
-$datos = json_decode($response, true);
-
-print_r($datos);
-
-if($datos['success'] == 1 && $datos['score'] >= 0.5){
-    if($datos['action'] == 'validarUsuario'){
-        
-    }
-    
-    } else {
-    echo "ERES UN ROBOT";
-}
-
+//Traigo datos del html
 $nombre = $_POST["name"];
 $email = $_POST["email"];
 $mensaje = $_POST["message"];
@@ -41,32 +15,61 @@ use PHPMailer\PHPMailer\Exception;
 //Load Composer's autoloader
 require 'vendor/autoload.php';
 
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
+//reCAPTCHA
+define('CLAVE', '6LfZP6wfAAAAAAotDhCCmHFHd91tkiNrKfVg5En-');
+	
+$token = $_POST['token'];
+$action = $_POST['action'];
 
-try {
+$cu = curl_init();
+curl_setopt($cu, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
+curl_setopt($cu, CURLOPT_POST, 1);
+curl_setopt($cu, CURLOPT_POSTFIELDS, http_build_query(array('secret' => CLAVE, 'response' => $token)));
+curl_setopt($cu, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($cu);
+curl_close($cu);
 
-    //Server settings
-    $mail->SMTPDebug = 0;                      //Enable verbose debug output
-    $mail->isSMTP();                           //Send using SMTP
-    $mail->Host       = 'smtp.zoho.com';       //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                  //Enable SMTP authentication
-    $mail->Username   = 'info@fosclean.com';   //SMTP username
-    $mail->Password   = 'Fos13915563';         //SMTP password
-    $mail->SMTPSecure = 'tls';                 //Enable implicit TLS encryption
-    $mail->Port       = 587;                   //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+$datos = json_decode($response, true);
 
-    //Recipients
-    $mail->setFrom('info@fosclean.com', $nombre);
-    $mail->addAddress('info@fosclean.com', $nombre);     //Add a recipient
+//print_r($datos);
 
-    //Content
-    $mail->isHTML(true);                        //Set email format to HTML
-    $mail->Subject = 'Mensaje desde formulario de contacto';
-    $mail->Body    = $body;
+if($datos['success'] == 1 && $datos['score'] >= 0.5){
+    if($datos['action'] == 'validarUsuario'){
+        
+    //Create an instance; passing `true` enables exceptions
+    $mail = new PHPMailer(true);
 
-    $mail->send();
-    echo "Thanks for sending your message! We'll get back to you shortly.";
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    try {
+
+        //Server settings
+        $mail->SMTPDebug = 0;                      //Enable verbose debug output
+        $mail->isSMTP();                           //Send using SMTP
+        $mail->Host       = 'smtp.zoho.com';       //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                  //Enable SMTP authentication
+        $mail->Username   = 'info@fosclean.com';   //SMTP username
+        $mail->Password   = 'Fos13915563';         //SMTP password
+        $mail->SMTPSecure = 'tls';                 //Enable implicit TLS encryption
+        $mail->Port       = 587;                   //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+        //Recipients
+        $mail->setFrom('info@fosclean.com', $nombre);
+        $mail->addAddress('info@fosclean.com', $nombre);     //Add a recipient
+
+        //Content
+        $mail->isHTML(true);                        //Set email format to HTML
+        $mail->Subject = 'Mensaje desde formulario de contacto';
+        $mail->Body    = $body;
+
+        $mail->send();
+        echo "Thanks for sending your message! We'll get back to you shortly.";
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+    }
+    
+
+
+    } else {
+    echo "ERES UN ROBOT";
 }
+
